c60ca32a8832e1743318375195457cc468e1d8ae
wjdwns <wjdwns3744@naver.com>
2021-02-02 22:13:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/actionbar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.actionbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setIcon(R.drawable.icon);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            Toast t = Toast.makeText(this,"Home AS UP Click",Toast.LENGTH_SHORT);
+            t.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
